84f07157e58c48c68e46b5852fc76e6720af34b8
Nathan Foulsham <nathan.foulsham@mindlinksoft.com>
2026-09-16 10:44:13 +0100

    It works!

--- Comments/X1.cs
+++ Comments/X1.cs
@@ -4,16 +4,15 @@ public class X1
 {
     public static int M(int lowerBound, int upperBound)
     {
-        int p = 0;
+        int accumulatedSum = 0;
 
-        // Iterate from lower bound (q) to upper bound (z)
         for (int i = lowerBound; i <= upperBound; i++)
         {
             // Add square of each number in the range
-            p += i * i;
+            accumulatedSum += i * i;
         }
 
         // Return accumulated sum
-        return p;
+        return accumulatedSum;
     }
 }
