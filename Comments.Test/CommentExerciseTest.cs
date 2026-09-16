3c9cf6effc39a1acaffeefc91e253727964c318c
Alex Pacey <alex.pacey@mindlinksoft.com>
2026-09-16 10:53:12 +0100

    It works!

--- Comments.Test/CommentExerciseTest.cs
+++ Comments.Test/CommentExerciseTest.cs
@@ -9,16 +9,16 @@ public class CommentExerciseTest
     public void AccumulativeSumOfSquaresInRangeTest()
     {
         int lowerBound = 7;
-        int b = 12;
+        int upperBound = 12;
 
         // Expected: sum of squares from 7 to 12
         int expected = 0;
-        for (int i = lowerBound; i <= b; i++)
+        for (int i = lowerBound; i <= upperBound; i++)
         {
             expected += i * i;
         }
 
-        int actual = CommentExercise.AccumulativeSumOfSquaresInRange(lowerBound, b);
+        int actual = CommentExercise.AccumulativeSumOfSquaresInRange(lowerBound, upperBound);
 
         Assert.That(actual, Is.EqualTo(expected));
     }
