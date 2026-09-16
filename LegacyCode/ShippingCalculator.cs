5551aa49269f14661a602b410e7ef550366e98a8
Nathan Foulsham <nathan.foulsham@mindlinksoft.com>
2026-09-16 12:04:44 +0100

    Refactor getting order into method

--- LegacyCode/ShippingCalculator.cs
+++ LegacyCode/ShippingCalculator.cs
@@ -21,30 +21,8 @@ public class ShippingCalculator
     {
         try
         {
-            var url = $"https://codemanship.co.uk/api/orders.php?orderId={orderId}";
+            var order = GetOrder(orderId);
 
-            var response = _httpClient
-                .GetAsync(url)
-                .GetAwaiter()
-                .GetResult();
-
-            response.EnsureSuccessStatusCode();
-
-            var json = response.Content
-                .ReadAsStringAsync()
-                .GetAwaiter()
-                .GetResult();
-
-            var options = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true
-            };
-            
-            var order = JsonSerializer.Deserialize<Order>(json, options);
-
-            if (order == null)
-                throw new Exception("Failed to deserialize order");
-            
             switch (order.ShippingType)
             {
                 case "STANDARD":
@@ -66,6 +44,34 @@ public class ShippingCalculator
             Console.WriteLine(e);
             return -1;
         }
+    }
+
+    private Order? GetOrder(int orderId)
+    {
+        var url = $"https://codemanship.co.uk/api/orders.php?orderId={orderId}";
+
+        var response = _httpClient
+            .GetAsync(url)
+            .GetAwaiter()
+            .GetResult();
+
+        response.EnsureSuccessStatusCode();
+
+        var json = response.Content
+            .ReadAsStringAsync()
+            .GetAwaiter()
+            .GetResult();
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+            
+        var order = JsonSerializer.Deserialize<Order>(json, options);
+
+        if (order == null)
+            throw new Exception("Failed to deserialize order");
+        return order;
     }
 }
 
