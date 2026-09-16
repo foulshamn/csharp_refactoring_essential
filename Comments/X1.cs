84a80918f9526b739470fc624887f2b395acf305
Nathan Foulsham <nathan.foulsham@mindlinksoft.com>
2026-09-16 10:43:02 +0100

    It works!

--- Comments/X1.cs
+++ Comments/X1.cs
@@ -10,16 +10,10 @@ public class X1
         for (int i = q; i <= z; i++)
         {
             // Add square of each number in the range
-            p += N(i);
+            p += i * i;
         }
 
         // Return accumulated sum
         return p;
-    }
-
-    static int N(int k)
-    {
-        // Return square of input
-        return k * k;
     }
 }
