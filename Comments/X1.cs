1cf62b4b2e4ed85f1d9709b7bb0f5fb81b98e4f5
Nathan Foulsham <nathan.foulsham@mindlinksoft.com>
2026-09-16 10:43:27 +0100

    It works!

--- Comments/X1.cs
+++ Comments/X1.cs
@@ -2,12 +2,12 @@
 
 public class X1
 {
-    public static int M(int q, int z)
+    public static int M(int lowerBound, int z)
     {
         int p = 0;
 
         // Iterate from lower bound (q) to upper bound (z)
-        for (int i = q; i <= z; i++)
+        for (int i = lowerBound; i <= z; i++)
         {
             // Add square of each number in the range
             p += i * i;
