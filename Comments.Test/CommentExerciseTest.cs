41e066437081e9b19609f6e66fec8bec94fea118
Alex Pacey <alex.pacey@mindlinksoft.com>
2026-09-16 10:57:10 +0100

    It works!

--- Comments.Test/CommentExerciseTest.cs
+++ Comments.Test/CommentExerciseTest.cs
@@ -8,12 +8,12 @@ public class CommentExerciseTest
     [Test]
     public void AccumulativeSumOfSquaresInRangeTest()
     {
-        int lowerBound = 7;
-        int upperBound = 12;
+        const int lowerBound = 7;
+        const int upperBound = 12;
 
-        int expected = 559;
+        const int expected = 559;
 
-        int actual = CommentExercise.AccumulativeSumOfSquaresInRange(lowerBound, upperBound);
+        var actual = CommentExercise.AccumulativeSumOfSquaresInRange(lowerBound, upperBound);
 
         Assert.That(actual, Is.EqualTo(expected));
     }
